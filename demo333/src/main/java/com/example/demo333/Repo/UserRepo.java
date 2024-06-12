628d0e7766c8475d5bcc7a02140a87d2677e3447
Qversite <125800775+Qversite@users.noreply.github.com>
2024-06-12 19:20:29 +0300

    Delete demo333 directory

--- demo333/src/main/java/com/example/demo333/Repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo333.Repo;
-
-import com.example.demo333.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Long> {
-    User findByUsername(String username);
-}
